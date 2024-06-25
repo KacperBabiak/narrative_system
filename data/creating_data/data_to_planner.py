48df01f802169abe6f66f2d3b2e7fe8c69716e2d
KacperBabiak <93874406+KacperBabiak@users.noreply.github.com>
2024-06-25 05:13:49 +0200

    module support
    new benchmark that is now created from modules

--- data/creating_data/data_to_planner.py
+++ data/creating_data/data_to_planner.py
@@ -17,302 +17,252 @@ def load_action(file):
 
 
 def create_file(characters,items,row):
-		with open("lib/island_new.txt", 'w') as f:
-			f.write("""/////////////////////////////////////types
-
-
+		with open("lib/boat_new.txt", 'w') as f:
+			f.write("""//### Types:
 type character;
-type item ;
+type ship;
+
+//### Properties:
+property satisfaction(char: character) : number; // Satisfaction level of each character
+property altruism(char: character) : number; // Altruism level of each character (-3 to 3)
+property ambition(char: character) : number; // Ambition level of each character (-3 to 3)
+property likes(char: character, other: character) : number; // Relationship liking level between characters (-3 to 3)
+property food(char: character) : number; // Food resource of each character
+property safety(char: character) : number; // Safety level of each character
+property fighting_ability(char: character) : number; // Fighting ability level of each character (-3 to 3)
+
+property ghost_relations(s :ship) : number; // Relationship level with ghosts (-3 to 3)
+property overall_food(s :ship) : number; // Overall food resource available
+property overall_coins(s :ship) : number; // Overall coins available
+
+property wants_to_survive(char: character) : boolean; 
+property wants_to_help_everyone(char: character) : boolean; 
+property wants_to_help_themself(char: character) : boolean; 
 
 
-//////////////////////////////////property
-
-
-//character
-
-
-property health(char: character) : number;
-property satisfaction(char: character) : number;
-
-property altruism(char: character) : number;
-property ambition(char: character) : number;
-
-property likes(char: character, other: character) : number;
-property wants_to_help(character : character, other: character) : boolean;
-property wants_to_attack(character : character, other: character) : boolean;
-property wants_item(character : character, item: item) : boolean;
-property wants_to_meet(character : character, other: character) : boolean;
-property wants_to_give(character : character,character2 : character, item: item) : boolean;
-property was_attacked(character : character) : boolean;
-property meeting(character : character, other: character) : boolean;
-property vulnerable(character : character) : boolean;
-property know_location(char:character,item:item) : boolean; 
-
-
-property has_item(char : character, item:item) : boolean;
-property quantity( item:item) : number;
-		   
-		   entity mc : character;
+entity mc : character;
+		   entity ship:ship;
 		   """
 			)
-			for item in items:
-				f.write("entity "+ item  + ": item;  \n")
-				f.write("quantity("+ item  + ") = " + str(row[item+"_quantity"]) + " ;  \n") 
+			
 
 				#warrtosci postaci
 			for char in characters:
 			
 				f.write("entity "+ char + ": character;  \n")
 				f.write("altruism(" + char  +") = " + str(row[char+"_altruism"]) + " ;\n")
+				f.write("ambition(" + char  +") = " + str(row[char+"_ambition"]) + " ;\n")
+				f.write("fighting_ability(" + char  +") = " + str(row[char+"_fighting_ability"]) + " ;\n")
+				f.write("safety(" + char  +") = " + str(row[char+"_safety"]) + " ;\n")
+				f.write("food(" + char  +") = " + str(row[char+"_food"]) + " ;\n")
 				
 				char_without_c = characters.copy()
 				char_without_c.remove(char)
 				for c2 in char_without_c:
 					f.write("likes(" + char  +", "+ c2 +") = " + str(row[char+"_likes_"+c2]) + " ;\n")
 				   
-				
-				for i in items:
-					if row[char+"_likes_"+c2] == 1:
-						f.write("has_item(" + char  +", "+ i +")   ;\n")
-				   
+				f.write("ghost_relations(ship) =" +str(row['ghost_relations']) + ";  \n")
+				f.write("overall_food(ship) =" +str(row['overall_food']) + " ; \n")
+				f.write("overall_coins(ship) =" +str(row['overall_coins']) + ";  \n")
 			
 
 				
 
 				
 
-			f.write("""action find(char : character,item : item) {
-	precondition:
-		quantity(item) > 0
-		
-		;
-		
+			f.write("""//# Action: survive
+action survive(char: character) {
+    precondition:
+        food(char) >= 0 // Character has no food
+        &safety(char) > 0
+        &char != mc; // Character's safety level is adequate
+    effect:
+        if (ambition(char)<=0)
+            satisfaction(char) = satisfaction(char) + 1; // Character gains satisfaction
+    consenting: char;
+};
 
-	effect:
-	
-	know_location(char,item) 
-	&quantity(item) = quantity(item) - 1
-	
-	;
-	consenting: char;
-	
+//# Action: help_everyone
+action help_everyone(char: character) {
+    precondition:
+        overall_food(ship) > 0 // Character has food
+       // &safety(char) < 0
+        &char != mc; // Character's safety level is low
+    effect:
+    if (ambition(char)>0 & altruism(char)>= 0)
+         satisfaction(char) = satisfaction(char) + 1; // Character gains satisfaction
+    consenting: char;
+};
+
+//# Action: help_themself
+action help_themself(char: character) {
+    precondition:
+        food(char) > 0 // Character has food
+        //&safety(char) < 0
+        &char != mc; // Character's safety level is low
+    effect:
+    if (ambition(char)>0 & altruism(char) < 0)
+         satisfaction(char) = satisfaction(char) + 1;// Character gains satisfaction
+    consenting: char;
+};
+
+//# Action: ghost_deal
+action ghost_deal(char: character) {
+    precondition:
+        ghost_relations(ship) > 0
+        &char != mc; // Character has good relations with ghosts
+    effect:
+        overall_food(ship) = overall_food(ship) + 1 // Character gains overall food
+        &safety(char) =  safety(char) - 1 // Character's safety level decreases
+        &ghost_relations(ship) = ghost_relations(ship) - 1; // Character's relations with ghosts decrease
+    consenting: char;
+};
+
+//# Action: fight_monster
+action fight_monster(char: character) {
+    precondition:
+        fighting_ability(char) > 0
+        &char != mc; // Character has high fighting ability
+    effect:
+        
+        overall_food(ship) = overall_food(ship) + 1
+        &fighting_ability(char) = fighting_ability(char) - 1 // Character's fighting ability decreases
+        &safety(char) = safety(char) - 1; // Character's safety level decreases
+    consenting: char;
+};
+
+//# Action: sacrifice_food
+action sacrifice_food(char: character) {
+    precondition:
+        food(char) > 0
+        &char != mc; // Character has food
+    effect:
+       
+        food(char) = food(char) -  1 // Character sacrifices food
+        &ghost_relations(ship) = ghost_relations(ship) + 1;// Character's relations with ghosts improve
+    consenting: char;
+};
+
+//# Action: sacrifice_money
+action sacrifice_money(char: character) {
+    precondition:
+        overall_coins(ship) > 0
+        &char != mc; // There are overall coins available
+    effect:
+        overall_coins(ship) = overall_coins(ship)- 1 // Overall coins decrease
+        &ghost_relations(ship) = ghost_relations(ship) + 1;  // Character's relations with ghosts improve
+    consenting: char;
+};
+
+//# Action: pray
+action pray(char: character, char2: character) {
+    precondition:
+        wants_to_survive(char2) 
+        &char != char2
+        &char != mc; // Character2's safety level is low
+    effect:
+       
+        ghost_relations(ship) = ghost_relations(ship) + 1; 
+    consenting: char;
+};
+
+//# Action: train_together
+action train_together(char: character, char2: character) {
+    precondition:
+        (likes(char2, char) > 1 | 
+        wants_to_help_everyone(char2))
+        &char != char2
+        &char != mc;
+    effect:
+        
+        fighting_ability(char) = fighting_ability(char) + 1 // Character's fighting ability increases
+        &fighting_ability(char2) =  fighting_ability(char) + 1 // Char2's fighting ability increases
+        &safety(char) = safety(char) + 1 // Character's safety level increases
+        &safety(char2) = safety(char2) + 1; // Char2's safety level increases
+    consenting: char;
 };
 
 
 
-action exchange_item(char : character, other: character, item1: item, item2 : item) {
-		precondition:
-			char != other
-			&char != mc
-			&item1 != item2
-			&has_item(char,item1)
-			&!has_item(other,item1)
-			&!has_item(char,item2)
-			&has_item(other,item2)
-			//&meeting(char,other)
-			
-			;
+//# Action: take_food
+action take_food(char: character, char2: character) {
+    precondition:
+        fighting_ability(char) > fighting_ability(char2) // Character has higher fighting ability than char2
+        &food(char2) > 0
+        &char != char2
+         &char != mc; // Char2 has food
+    effect:
+        
+        food(char) = food(char) + 1 // Character gains food
+        &food(char2) = food(char2) - 1 // Char2 loses food
+        &likes(char2, char) = likes(char2, char) -  1 // Relationship liking decreases between char and char2
+        &safety(char) = safety(char) -  1 
+        &safety(char2) = safety(char2) - 1; // Character's safety level decreases
+    consenting: char;
+};
 
-		effect:
-			
-			wants_to_give(char,other,item1)
-			&wants_to_give(other,char,item2)
-			
-			;
-		consenting: char,other;
-		
-	};
+//# Action: threaten
+action threaten(char: character, char2: character) {
+    precondition:
+        wants_to_survive(char2) 
+        &char != char2
+        &char != mc; 
+    effect:
+        
+        fighting_ability(char2) = fighting_ability(char2) - 1 // Character's fighting ability increases
+        &likes(char2, char) = likes(char2, char) - 1; // Relationship liking decreases between char and char2
+    consenting: char;
+};
 
-action give_item(char : character, other: character, item: item) {
-		precondition:
-			char != other
-			&char != mc
-			&has_item(char,item)
-			&!has_item(other,item)
-			//&meeting(char,other)
-			
-			;
+//# Action: weaken
+action weaken(char: character, char2: character) {
+    precondition:
+    char != char2
+    &char != mc;
+    effect:
+         fighting_ability(char2) = fighting_ability(char2) - 1 // Character's fighting ability increases
+        &likes(char2, char) = likes(char2, char) - 1; // Relationship liking decreases between char and char2
+    consenting: char;
+};
 
-		effect:
-			
-			wants_to_help(other,char) 
-			&wants_to_give(char,other,item)
+//# Action: safe_alone
+action safe_alone(char: character) {
+    precondition:
+    char != mc;
+    effect:
+        safety(char) = safety(char) + 1; // Character's safety level increases
+    consenting: char;
+};
 
-			& likes(other,char) = likes(other,char) + 1
-			;
-		consenting: char,other;
-		
-	};
+//# Action: safe_together
+action safe_together(char: character, char2: character) {
+    precondition:
+        wants_to_survive(char2)
+        &char != char2
+        &char != mc; // Char2 has no food
+    effect:
+        safety(char) =  safety(char) +1 // Character's safety level increases
+        &safety(char2) =  safety(char2) +1; // Char2's safety level increases
+    consenting: char;
+};
 
-
-action prepare_duel(char : character, other: character) {
-		precondition:
-			char != other
-			&char != mc
-			&wants_to_attack(other,char) 
-			
-			;
-
-		effect:
-			
-			vulnerable(other)
-			
-			;
-		consenting: char;
-		
-	};
-
-
-
-
-action prepare_ally(char : character, other: character, attacked : character) {
-		precondition:
-			char != other
-			&attacked != other
-			& attacked != char
-			&char != mc
-			&wants_to_attack(char,attacked)
-			&(wants_to_attack(other,attacked) | wants_to_help(other,char)  | likes(other, char) == 2 | wants_to_attack(attacked,other)) 
-			;
-
-		effect:
-			
-			vulnerable(attacked)
-			&wants_to_attack(other,attacked)
-			//&wants_to_attack(char,attacked)
-			& likes(char,other) = likes(char,other) + 1
-			;
-		consenting: char, other;
-		
-	};
-
-
-action attack(char : character, other: character) {
-		precondition:
-			char != other
-			&char != mc
-			&vulnerable(other)  
-			//&meeting(char,other)
-			
-			;
-
-		effect:
-			!vulnerable(other)  
-			&was_attacked(other)
-			&if(wants_to_attack(char,other) | (altruism(char) + likes(other,char) < 0))  satisfaction(char) = satisfaction(char) + 1
-			&if( (altruism(char) + likes(other,char) > 3))  satisfaction(char) = satisfaction(char) - 1
-			;
-		consenting: char;
-		
-	};
-
-action get_item(char : character, other: character, item : item) {
-		precondition:
-			char != other
-			&char != mc
-		   & ((has_item(other,item) & (was_attacked(other) | wants_to_give(other,char,item)) )
-			| know_location(char,item) 
-		   )
-			//| ambition(char) > 2
-			//&meeting(char,other)
-			&!has_item(char,item)
-			
-			//&!vulnerable(char)
-			;
-
-		effect:
-			has_item(char,item)
-			&!has_item(other,item)
-			& if(!has_item(char,item)) satisfaction(char) = satisfaction(char) + 1
-			& if(vulnerable(other)) satisfaction(char) = satisfaction(char) - 1
-			;
-		consenting: char;
-		
-	};
-
-action prepare_deal(char : character, other: character, item: item) {
-		precondition:
-			char != other
-			&char != mc
-			&has_item(char,item)
-			&!has_item(other,item)
-			//&meeting(char,other)
-			
-			;
-
-		effect:
-			meeting(char,other)
-			&meeting(other,char)
-			//vulnerable(other)
-			&wants_to_give(char,other,item)
-			;
-		consenting: char,other;
-		
-	};
-
-action prepare_meeting(char : character, other: character) {
-		precondition:
-			char != other
-			&char != mc
-			&!meeting(char,other)
-			&!meeting(other,char)
-			;
-
-		effect:
-			
-			meeting(char,other)
-			&meeting(other,char)
-			& likes(other,char) = likes(other,char) + 1
-			& if((altruism(other) + likes(char,other)>2) | wants_to_meet(other,char)) satisfaction(other) = satisfaction(other) + 1
-			;
-		consenting: char,other;
-		
-	};
-
-action prepare_trap(char : character, other: character, meeted : character) {
-		precondition:
-			//char != other
-			char != mc
-			& char != meeted
-			&meeting(other,meeted)
-			;
-
-		effect:
-			
-			vulnerable(other)
-			
-			;
-		consenting: char;
-		
-	};
-
-
-
-action take_care_of(char : character, other: character) {
-		precondition:
-			
-			char != mc
-			
-			//&vulnerable(other)
-			&was_attacked(other)
-			;
-
-		effect:
-			
-			!was_attacked(other)
-			//&meeting(char,other)
-			//&meeting(other,char)
-			& if(((altruism(other) + likes(char,other)>2) | wants_to_meet(other,char)) & char != other) satisfaction(char) = satisfaction(char) + 1
-			;
-		consenting: char;
-		
-	};
+//# Action: accuse
+action accuse(char: character, char2: character) {
+    precondition:
+        wants_to_help_themself(char2)
+        &char != char2
+        &char != mc; // Character has no food
+    effect:
+        food(char) = 0
+        &likes(char2, char) = likes(char2, char) - 1; // Relationship liking increases between char and char2
+    consenting: char;
+};
 			""")
 
 			
-			acting_char = row['active']
+			
 			f.write("utility(): \n ")
-			f.write("satisfaction({}) ;\n".format(acting_char))
+			f.write("satisfaction({}) ;\n".format("char_acting"))
 
 			
 		
@@ -325,20 +275,20 @@ action take_care_of(char : character, other: character) {
 			
 			f.close()
 
-characters = ['doctor', 'soldier', 'actor']
-items = ['food', 'meds']
-file = 'lib/island_new.txt'
+characters = ['char_acting', 'char1', 'char2']
+items = ['food']
+file = 'lib/boat_new.txt'
 
-df = pd.read_csv('data\all_states_balanced.csv')
+df = pd.read_csv('data\\creating_data\\random_states.csv')
 
-for index in range(1252,len(df)):
+for index in range(0,len(df)):
 	row=df.loc[index]
 	create_file(characters, items, row)
 
 	action = load_action(file)
 	print(index)
 	df.loc[index,['results']] = action
-	df.to_csv('data/all_states_results_balanced.csv')
+	df.to_csv('data/random_states_results.csv')
 	
 
 print(df)
