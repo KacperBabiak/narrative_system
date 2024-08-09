bb2ce24ec10e8eeb6dcf1dc5e234e87ca588dc76
KacperBabiak <93874406+KacperBabiak@users.noreply.github.com>
2024-08-09 13:34:13 +0200

    testing

--- prototype/create_dataset.py
+++ prototype/create_dataset.py
@@ -19,7 +19,7 @@ def load_action(file):
 
 def create_file(characters,items,places,utilities,row):
 		
-		acting_character = str(row["acting_character"])
+		acting_character = str(row["acting_char"])
 		with open("prototype\\basketball_prototype.txt", 'w') as f:
 			f.write("""type item;
 type place;
@@ -90,62 +90,63 @@ entity Murder : crime;
 				
 
 			f.write("""
-action travel(character : character, from : place, to : place){
+action travel(character : character, from : place, to : place){{
 	precondition:
 		from != to & 
 		at(character) == from &
-		alive(character);
+		alive(character)
+		&character == {0};
 	effect:
 		at(character) = to;
 	consenting: character; 
 	observing(c : character) : at(c) == from | at(c) == to; 
-};
+}};
 
-action arrest(police : police, character : character, place : place, crime : crime){
+action arrest(police : police, character : character, place : place, crime : crime){{
 	precondition: 
 		at(police) == place &
 		at(character) == place &
 		police != character &
 		alive(police) &
 		alive(character) &
-		suspect(character, crime);
+		suspect(character, crime)&police == {0};
 	effect:
 		underArrest(character) = 1;
 	consenting: police;
 	observing(a : character) : at(a) == place;
-};
+}};
 
-action steal(thief : citizen, victim : citizen, item : item, place : place){
+action steal(thief : citizen, victim : citizen, item : item, place : place){{
 	precondition:
 		at(thief) == place &
 		at(victim) == place &
 		has(item) == victim &
 		thief != victim &
-		alive(thief);
+		alive(thief)&thief == {0};
 	effect:
 		has(item) = thief &
 		angry(victim) = 1 &
 		clue(Theft, item, place);
 	consenting: thief;
 	observing(c : character) : (c == thief | c == victim) | (at(c) == place & place != Downtown); // crimes downtown aren't observed
-};
+}};
 
-action play_basketball(player1 : citizen, player2 : citizen, place : basketballPlace){
+action play_basketball(player1 : citizen, player2 : citizen, place : basketballPlace){{
 	precondition:
 		player1 != player2 &
 		at(player1) == place &
 		alive(player1) &
 		at(player2) == place &
 		alive(player2) &
-		has(Basketball) == player1;
+		has(Basketball) == player1 & player1 == {0};
 	effect:
 		angry(player1) = 0 &
 		angry(player2) = 0;
 	consenting: player1;
 	observing(c : character) : at(c) == place;
-};
+}};
 
-action kill(killer : citizen, victim : citizen, item : item, place : place){
+action kill(killer : citizen, victim : citizen, item : item, place : place){{
 	precondition:
 		killer != victim &
 		at(killer) == place &
@@ -153,57 +154,57 @@ action kill(killer : citizen, victim : citizen, item : item, place : place){
 		alive(killer) &
 		alive(victim) &
 		has(item) == killer &
-		underArrest(killer) == 0;
+		underArrest(killer) == 0 & killer == {0};
 	effect:
 		!alive(victim) &
 		clue(Murder, item, place);
 	consenting: killer;
 	observing(c : character) : c == killer | (at(c) == place & place != Downtown); 
-};
+}};
 	
-action find_clues(police : police, crime : crime, item : item, place : place){
+action find_clues(police : police, crime : crime, item : item, place : place){{
 	precondition:
 		at(police) == place &
 		alive(police)
-		&clue(crime, item, place);
+		&clue(crime, item, place) &police == {0};
 	effect:
 		searched(place) = 1 &
 		if(clue(crime, item, place))
 			believes(police, clue(crime, item, place));
 	consenting: police;
 	observing(c : character) : at(c) == place;
-};
+}};
 
-action share_clues(police1 : police, police2 : police, crime : crime, item : item, place : place){
+action share_clues(police1 : police, police2 : police, crime : crime, item : item, place : place){{
 	precondition:
 		police1 != police2 &
 		at(police1) == place &
 		alive(police1) &
 		at(police2) == place &
 		alive(police2) &
-		clue(crime, item, place);
+		clue(crime, item, place) &police1 == {0} ;
 	effect:
 		believes(police2, clue(crime, item, place));
 	consenting: police1;
 	observing(c : character) : at(c) == place;
-};
+}};
  
-action suspect_of_crime(police : police, citizen : citizen, crime : crime, item : item, place : place){
+action suspect_of_crime(police : police, citizen : citizen, crime : crime, item : item, place : place){{
 	precondition:
 		police != citizen &
 		at(police) == place &
 		alive(police) &
 		at(citizen) == place &
 		alive(citizen) &
-		has(item) == citizen &
+		has(item) == citizen
+		&police == {0} &
 		exists(p : place) clue(crime, item, p);
 	effect:
 		suspect(citizen, crime);
 	consenting: police;
 	observing(c : character) : at(c) == place;
-};
-
-trigger see_has(character : character, other : character, item : item, place : place){
+}};
+trigger see_has(character : character, other : character, item : item, place : place){{
 	precondition:
 		at(character) == place &
 		at(other) == place &
@@ -211,9 +212,9 @@ trigger see_has(character : character, other : character, item : item, place : p
 		believes(character, has(item) != other);
 	effect:
 		believes(character, has(item) = other);
-};
+}};
 
-trigger see_hasnt(character : character, other : character, item : item, place : place){
+trigger see_hasnt(character : character, other : character, item : item, place : place){{
 	precondition:
 		at(character) == place &
 		at(other) == place &
@@ -221,28 +222,27 @@ trigger see_hasnt(character : character, other : character, item : item, place :
 		believes(character, has(item) == other);
 	effect:
 		believes(character, has(item) = ?);
-};
+}};
 
-trigger see_at(character : character, other : character, place : place){
+trigger see_at(character : character, other : character, place : place){{
 	precondition:
 		at(character) == place &
 		at(other) == place &
 		believes(character, at(other) != place);
 	effect:
 		believes(character, at(other) = place);
-};
+}};
 
-trigger see_gone(character : character, other : character, place : place){
+trigger see_gone(character : character, other : character, place : place){{
 	precondition:
 		at(character) == place &
 		at(other) != place &
 		believes(character, at(other) == place);
 	effect:
 		believes(character, at(other) = ?);
-};
+}};
 
-			""")
-		   #.format(acting_character)
+			""".format(str(acting_character)))
 
 			
 			
@@ -272,7 +272,7 @@ utilities = {
 }
 file = 'prototype\\basketball_prototype.txt'
 
-df = pd.read_csv('prototype\\table_training.csv')
+df = pd.read_csv('prototype\\table_training_simple.csv')
 
 for index in range(0,len(df)):
 	print(index)
@@ -293,7 +293,7 @@ for index in range(0,len(df)):
 	print(action)
 	print(end-start)
 
-	df.to_csv('dataset_results.csv',index=False)
+	df.to_csv('prototype\dataset_results.csv',index=False)
 
 	
 	
